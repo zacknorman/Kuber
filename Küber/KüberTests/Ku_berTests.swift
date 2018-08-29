2401bfcbab5a38d2ab600fb957bafdbd5c305438
Zack Norman <zachary.norman@mail.utoronto.ca>
2018-08-29 15:10:35 -0400

    CODE
    DONT TOUCH THESE FILES

---
+++ Küber/KüberTests/Ku_berTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ku_berTests.swift
+//  KüberTests
+//
+//  Created by Zack Norman on 2018-08-14.
+//  Copyright © 2018 Norman LLC. All rights reserved.
+//
+
+import XCTest
+@testable import Ku_ber
+
+class Ku_berTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
